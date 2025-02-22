d3d034a419a21ce3303a1b5fba92e0214bc2f4f3
ghitamerad <ghitamerad5@gmail.com>
2025-02-22 17:16:45 +0100

    done

--- database/migrations/2025_02_18_215725_add_en_stock_to_modeles_table.php
+++ database/migrations/2025_02_18_215725_add_en_stock_to_modeles_table.php
@@ -4,16 +4,25 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
-    public function up(): void {
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
         Schema::table('modeles', function (Blueprint $table) {
-            $table->boolean('en_stock')->default(true)->after('xml');
+            //
         });
     }
 
-    public function down(): void {
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
         Schema::table('modeles', function (Blueprint $table) {
-            $table->dropColumn('en_stock');
+            //
         });
     }
 };
