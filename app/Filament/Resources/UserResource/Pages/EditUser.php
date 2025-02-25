42fe868e748514d711ce7c3e15ae11907fe477af
ghitamerad <ghitamerad5@gmail.com>
2025-02-25 11:29:48 +0100

    edit

--- app/Filament/Resources/UserResource/Pages/EditUser.php
+++ app/Filament/Resources/UserResource/Pages/EditUser.php
@@ -1,15 +1,30 @@
 <?php
 
 namespace App\Filament\Resources\UserResource\Pages;
-
+ 
 use App\Filament\Resources\UserResource;
+use App\Http\Controllers\UserController;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
-
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+ 
 class EditUser extends EditRecord
 {
     protected static string $resource = UserResource::class;
-
+ 
+    protected function mutateFormDataBeforeSave(array $data): array
+    {
+        // Récupérer l'utilisateur
+        $user = $this->record;
+ 
+        // Appeler la méthode update du UserController
+        $request = new Request($data);
+        app(UserController::class)->update($request, $user);
+ 
+        return $data;
+    }
+ 
     protected function getHeaderActions(): array
     {
         return [
